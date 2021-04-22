4e764bea20fbc57cdef1a824c2bae439fe9e489e
Yuri Hallan <yurihallan9@gmail.com>
2021-04-22 19:59:34 -0400

    Modificacao no controller das requisicoes http.

--- src/main/java/br/com/api/contatos/controller/ContatosController.java
+++ src/main/java/br/com/api/contatos/controller/ContatosController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.api.contatos.entity.Contatos;
 import br.com.api.contatos.repository.ContatosRepository;
 
+@CrossOrigin(origins = "http://localhost:9000")
 @RestController
 public class ContatosController {
     
@@ -31,7 +33,7 @@ public class ContatosController {
     }
     // metodo GET - listando um contato especifico
     @RequestMapping(value = "/contatos/{id}", method = RequestMethod.GET)
-    public ResponseEntity<Contatos> GetById(@PathVariable(value = "id") long id){
+    public ResponseEntity<Contatos> Get(@PathVariable(value = "id") long id){
         Optional<Contatos> contato = _ContatosRepository.findById(id);
         if(contato.isPresent()){
             return new ResponseEntity<Contatos>(contato.get(),HttpStatus.OK);
@@ -53,6 +55,9 @@ public class ContatosController {
         if(oldContato.isPresent()){
             Contatos contato = oldContato.get();
             contato.setNome(NewContato.getNome());
+            contato.setSexo(NewContato.getSexo());
+            contato.setTelefone(NewContato.getTelefone());
+            contato.setEmail(NewContato.getEmail());
             _ContatosRepository.save(contato);
             return new ResponseEntity<Contatos>(contato, HttpStatus.OK);
         }else{
